07b83bcb5d2514499b2f55a48ebab72fad920bc3
Hubert Banczerowski <h.banczerowski@gmail.com>
2014-11-28 14:26:32 +0100

    The menu option for Cars/Citroen/Xara page

--- src/Gajdaw/BDDTutorial/CarsBundle/Controller/CitroenController.php
+++ src/Gajdaw/BDDTutorial/CarsBundle/Controller/CitroenController.php
@@ -9,10 +9,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class CitroenController extends Controller
 {
     /**
-     * @Route("/cars/citroen/c4.html")
+     * @Route("/cars/citroen/c4.html", name="cars-citroen-c4")
      * @Template()
      */
     public function c4Action()
+    {
+        return array();
+    }
+    
+    
+    /**
+     * @Route("/cars/citroen/xara.html", name="cars-citroen-xara")
+     * @Template()
+     */
+    public function xaraAction()
     {
         return array();
     }
